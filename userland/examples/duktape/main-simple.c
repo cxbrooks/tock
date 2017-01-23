1d7630322249d5f56133e14f887597be3bc861fc
Christopher Brooks <cxh@eecs.berkeley.edu>
2017-01-23 13:16:16 -0800

    Updated putstr() calls to use main-simple.c

--- userland/examples/duktape/main-simple.c
+++ userland/examples/duktape/main-simple.c
@@ -40,35 +40,35 @@ static duk_ret_t native_adder(duk_context *ctx) {
 }
 
 int main(int argc, char *argv[]) {
-  putstr("duktape/main.c: start\n");
+  putstr("duktape/main-simple.c: start\n");
   duk_context *ctx = duk_create_heap_default();
-  putstr("duktape/main.c: after heap create\n");
+  putstr("duktape/main-simple.c: after heap create\n");
 
   (void) argc; (void) argv;  /* suppress warning */
 
-  putstr("duktape/main.c: before pushing native_print 2\n");
+  putstr("duktape/main-simple.c: before pushing native_print 2\n");
   duk_push_c_function(ctx, native_print, DUK_VARARGS);
-  putstr("duktape/main.c: after pushing native_print\n");
+  putstr("duktape/main-simple.c: after pushing native_print\n");
 
   duk_put_global_string(ctx, "print");
-  putstr("duktape/main.c: after put_global print\n");
+  putstr("duktape/main-simple.c: after put_global print\n");
 
   duk_eval_string(ctx, "print('Hello world!');");
-  putstr("duktape/main.c: after eval print1\n");
+  putstr("duktape/main-simple.c: after eval print1\n");
 
   duk_push_c_function(ctx, native_adder, DUK_VARARGS);
-  putstr("duktape/main.c: after pushing native_adder\n");
+  putstr("duktape/main-simple.c: after pushing native_adder\n");
 
   duk_put_global_string(ctx, "adder");
-  putstr("duktape/main.c: after put_global print\n");
+  putstr("duktape/main-simple.c: after put_global print\n");
 
 
   duk_eval_string(ctx, "print('2+3=' + adder(2, 3));");
-  putstr("duktape/main.c: after eval print2\n");
+  putstr("duktape/main-simple.c: after eval print2\n");
 
   duk_pop(ctx);  /* pop eval result */
 
   duk_destroy_heap(ctx);
-  putstr("duktape/main.c: done\n");
+  putstr("duktape/main-simple.c: done\n");
   return 0;
 }
